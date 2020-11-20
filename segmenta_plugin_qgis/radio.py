7a15bd004e3adc35260047326a44b337eec8097d
Bichav <veronicaheredia9@gmail.com>
2020-11-20 13:10:42 -0300

    se modifica el archivo .py  en plugins

--- segmenta_plugin_qgis/radio.py
+++ segmenta_plugin_qgis/radio.py
@@ -6,111 +6,23 @@ from PyQt5.QtWidgets import QAction, QDialog, QFormLayout
 
 #Añadiendo capas PostGIS
 uri = QgsDataSourceUri()
+ 
 # introducimos nombre del servidor, puerto, nombre de la base de datos, usuario y contraseña
-uri.setConnection("172.26.68.174", "5432", "seg", "vhere", "vero_1234")
+uri.setConnection(dbHost,dbPort,dbName,dbUsr[0],dbPwd[0])
 
+
+#Conexion PostGIS
+uri = QgsDataSourceUri()
+# introducimos nombre del servidor, puerto, nombre de la base de datos, usuario y contraseña
+uri.setConnection(dbHost,dbPort,dbName,dbUsr[0],dbPwd[0])
 
 origen = QInputDialog.getText(None, 'origen', 'Introduce la ruta de acceso')
 aglomerado = QInputDialog.getText(None, 'aglomerado', 'Introduce el nombre completo del aglomerado')
 
-  
-
-# Agrego la capa  Segmento
-uri.setDataSource(aglomerado[0], "arc" , "wkb_geometry" )
-layer = QgsVectorLayer(uri.uri(), "segmentos", "postgres")
-if not layer.isValid():
-    print ("el numero de aglomerado no es correcto")
-QgsProject.instance().addMapLayer(layer)
-renderer = layer.renderer()
-layer.loadNamedStyle(origen[0] + '\estilo_radio\segmentos.qml')
-iface.mapCanvas().refresh() 
-QgsProject.instance().mapLayers().values()
-layer.triggerRepaint()
 
 
-# Agrego la capa  Mascara desde BD
-#uri.setDataSource(aglomerado[0] , "radios" , "wkb_geometry" )
-#layer = QgsVectorLayer(uri.uri(), "mascara", "postgres")
-#if not layer.isValid():
-#    print ("no existe capa radio")
-#QgsProject.instance().addMapLayer(layer)
-#renderer = layer.renderer()
-#layer.loadNamedStyle(origen[0] +'\estilo_radio\mascara.qml')
-#iface.mapCanvas().refresh() 
-#QgsProject.instance().mapLayers().values()
-#layer.triggerRepaint()
 
-
-# Agrego la capa  MASCARA RADIOS desde carpeta censo2020
-capa = origen[0]+'\humahuaca\pradio' + '.shp'
-nomcapa = 'mascara' 
-#print capa
- ##carga la capa
-layer = QgsVectorLayer(capa,nomcapa,'ogr')
-if not layer.isValid():
-    print ("la capa no es correcta")
-QgsProject.instance().addMapLayer(layer)
-renderer = layer.renderer()
-layer.loadNamedStyle(r''+origen[0]+'\estilo_radio\mascara.qml')
-iface.mapCanvas().refresh() 
-QgsProject.instance().mapLayers().values()
-layer.triggerRepaint()
-
-
-# Agrego la capa  Especiales
-uri.setDataSource(aglomerado[0], "arc" , "wkb_geometry" )
-layer = QgsVectorLayer(uri.uri(), "especiales", "postgres")
-if not layer.isValid():
-    print ("el numero de aglomerado no es correcto")
-QgsProject.instance().addMapLayer(layer)
-renderer = layer.renderer()
-layer.loadNamedStyle(origen[0] + '\estilo_radio\especiales.qml')
-iface.mapCanvas().refresh() 
-QgsProject.instance().mapLayers().values()
-layer.triggerRepaint()
-
-# Agrego la capa  Radios desde BD
-#uri.setDataSource(aglomerado[0] , "radios" , "wkb_geometry" )
-#layer = QgsVectorLayer(uri.uri(), "radios", "postgres")
-#if not layer.isValid():
-#    print ("no existe capa radio")
-#QgsProject.instance().addMapLayer(layer)
-#renderer = layer.renderer()
-#layer.loadNamedStyle(origen[0] +'\estilo_radio\radio.qml')
-##iface.mapCanvas().refresh() 
-#QgsProject.instance().mapLayers().values()
-#layer.triggerRepaint()
-
-
-# Agrego la capa  RADIOS desde carpeta censo2020
-capa = origen[0]+'\humahuaca\pradio' + '.shp'
-nomcapa = 'radios' 
-#print capa
- ##carga la capa
-layer = QgsVectorLayer(capa,nomcapa,'ogr')
-if not layer.isValid():
-    print ("la capa no es correcta")
-QgsProject.instance().addMapLayer(layer)
-renderer = layer.renderer()
-layer.loadNamedStyle(origen[0]+'\estilo_radio\pradio.qml')
-iface.mapCanvas().refresh() 
-QgsProject.instance().mapLayers().values()
-layer.triggerRepaint() 
-
-# Agrego la capa Etiquetas Manzanas  
-uri.setDataSource(aglomerado[0] , "lab" , "wkb_geometry" )
-layer = QgsVectorLayer(uri.uri(), "etiqueta_manzana", "postgres")
-if not layer.isValid():
-    print ("el numero de aglomerado no es correcto")
-QgsProject.instance().addMapLayer(layer)
-renderer = layer.renderer()
-layer.loadNamedStyle(origen[0] +'\estilo_radio\manzanas.qml')
-iface.mapCanvas().refresh() 
-QgsProject.instance().mapLayers().values()
-layer.triggerRepaint() 
-
-# Agrego las tablas desde la carpeta censo2020
-
+# Agrego las tablas desde la carpeta salidagrafica
 # Agrego tabla provincia
 capa = origen[0] + '\datos_prov\provincia.csv'
 nomcapa = 'provincia'  
@@ -143,5 +55,100 @@ if not layer.isValid():
     print ("la capa no es correcta")
 QgsProject.instance().addMapLayer(layer)
 renderer = layer.renderer()
+
+# Agrego todas las capas al proyecto
+# Agrego la capa  Segmento
+uri.setDataSource(aglomerado[0], "arc" , "wkb_geometry" )
+layer = QgsVectorLayer(uri.uri(), "segmentos", "postgres")
+if not layer.isValid():
+    print ("el numero de aglomerado no es correcto")
+QgsProject.instance().addMapLayer(layer)
+renderer = layer.renderer()
+layer.loadNamedStyle(origen[0] + '\estilo_radio\segmentos.qml')
+iface.mapCanvas().refresh() 
+QgsProject.instance().mapLayers().values()
+layer.triggerRepaint()
+
+
+#Agrego la capa  Mascara desde BD
+uri.setDataSource(aglomerado[0] , "radios" , "wkb_geometry" )
+layer = QgsVectorLayer(uri.uri(), "mascara", "postgres")
+if not layer.isValid():
+    print ("no existe capa radio")
+QgsProject.instance().addMapLayer(layer)
+renderer = layer.renderer()
+layer.loadNamedStyle(origen[0] +'\estilo_radio\mascara.qml')
+iface.mapCanvas().refresh() 
+QgsProject.instance().mapLayers().values()
+layer.triggerRepaint()
+
+
+# Agrego la capa  MASCARA RADIOS desde carpeta censo2020
+#capa = origen[0]+'\humahuaca\pradio' + '.shp'
+#nomcapa = 'mascara' 
+#print capa
+ ##carga la capa
+#layer = QgsVectorLayer(capa,nomcapa,'ogr')
+#if not layer.isValid():
+    #print ("la capa no es correcta")
+#QgsProject.instance().addMapLayer(layer)
+#renderer = layer.renderer()
+#layer.loadNamedStyle(r''+origen[0]+'\estilo_radio\mascara.qml')
+#iface.mapCanvas().refresh() 
+#QgsProject.instance().mapLayers().values()
+#layer.triggerRepaint()
+
+
+# Agrego la capa  Especiales
+uri.setDataSource(aglomerado[0], "arc" , "wkb_geometry" )
+layer = QgsVectorLayer(uri.uri(), "especiales", "postgres")
+if not layer.isValid():
+    print ("el numero de aglomerado no es correcto")
+QgsProject.instance().addMapLayer(layer)
+renderer = layer.renderer()
+layer.loadNamedStyle(origen[0] + '\estilo_radio\especiales.qml')
+iface.mapCanvas().refresh() 
+QgsProject.instance().mapLayers().values()
+layer.triggerRepaint()
+
+#Agrego la capa  Radios desde BD
+uri.setDataSource(aglomerado[0] , "radios" , "wkb_geometry" )
+layer = QgsVectorLayer(uri.uri(), "radios", "postgres")
+if not layer.isValid():
+    print ("no existe capa radio")
+QgsProject.instance().addMapLayer(layer)
+renderer = layer.renderer()
+layer.loadNamedStyle(origen[0] +'\estilo_radio\radio.qml')
+#iface.mapCanvas().refresh() 
+QgsProject.instance().mapLayers().values()
+layer.triggerRepaint()
+
+
+# Agrego la capa  RADIOS desde carpeta censo2020
+#capa = origen[0]+'\humahuaca\pradio' + '.shp'
+#nomcapa = 'radios' 
+#print capa
+ ##carga la capa
+#layer = QgsVectorLayer(capa,nomcapa,'ogr')
+#if not layer.isValid():
+   # print ("la capa no es correcta")
+#QgsProject.instance().addMapLayer(layer)
+#renderer = layer.renderer()
+#layer.loadNamedStyle(origen[0]+'\estilo_radio\pradio.qml')
+#iface.mapCanvas().refresh() 
+#QgsProject.instance().mapLayers().values()
+#layer.triggerRepaint() 
+
+# Agrego la capa Etiquetas Manzanas  
+uri.setDataSource(aglomerado[0] , "lab" , "wkb_geometry" )
+layer = QgsVectorLayer(uri.uri(), "etiqueta_manzana", "postgres")
+if not layer.isValid():
+    print ("el numero de aglomerado no es correcto")
+QgsProject.instance().addMapLayer(layer)
+renderer = layer.renderer()
+layer.loadNamedStyle(origen[0] +'\estilo_radio\manzanas.qml')
+iface.mapCanvas().refresh() 
+QgsProject.instance().mapLayers().values()
+layer.triggerRepaint() 
 
 
