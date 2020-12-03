9903f93f77b9cccc91bcb49392975fce8013550c
Bichav <veronicaheredia9@gmail.com>
2020-12-03 07:14:41 -0300

    cambio conexion.py

--- conexionbd.py
+++ conexionbd.py
@@ -1,14 +1,21 @@
 from PyQt5.QtCore import QSettings, QTranslator, qVersion, QVersionNumber, QCoreApplication, Qt, QObject, pyqtSignal
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QAction, QDialog, QFormLayout 
+
 qs = QSettings()
 dbHost = qs.value("PostgreSQL/connections/informatica/host")
 dbPort = qs.value("PostgreSQL/connections/informatica/port")
 dbName = qs.value("PostgreSQL/connections/informatica/database")
-
-#dbHost = qs.value("PostgreSQL/connections/segmenter/host")
-#dbPort = qs.value("PostgreSQL/connections/segmenter/port")
-#dbName = qs.value("PostgreSQL/connections/segmenter/database")
+#Pedir al usuario cargar los campos de  usuario y contraseña
 dbUsr = QInputDialog.getText(None, 'usuario', 'Introduce el nombre de usuario de la base de datos')
 dbPwd = QInputDialog.getText(None, 'contraseña', 'Introduce la contraseña')
 
+
+#Barra de carga
+#import time
+#progressMessageBar = iface.messageBar().createMessage("espere mientras se carga el proyecto...")
+#progress = QProgressBar()
+#progress.setMaximum(10)
+#progress.setAlignment(Qt.AlignLeft|Qt.AlignVCenter)
+#progressMessageBar.layout().addWidget(progress)
+#iface.messageBar().pushWidget(progressMessageBar, Qgis.Info)
